b320166134ce76b630e6c8cd37a7e4ec7337a231
AbdulovADA <lil.vlad7608@gmail.com>
2022-04-24 20:50:32 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+int originAccount = 200;
+int deposit = 700;
+int account = originAccount + deposit;
+int bonus1 = 100;
+int bonus = 0;
+if (deposit > 1000) {
+    bonus = deposit / bonus1;
+}
+int finalAccount = originAccount + deposit + bonus;
+
+        System.out.println("Ваш баланс:" + finalAccount);
+        System.out.println("Количество бонусных рублей:" + bonus);
+    }
+}
