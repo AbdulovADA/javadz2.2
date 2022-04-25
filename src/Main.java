4d7c8a7ab081a892ba8c54efb243fe552729afe3
AbdulovADA <lil.vlad7608@gmail.com>
2022-04-25 20:50:43 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int originAccount = 200;
-        int deposit = 900;
+        int deposit = 1100;
         int account = originAccount + deposit;
         int bonus1 = 100;
         int bonus = 0;
-        if (deposit > 1000) ;
-        {
+        if (deposit > 1000) {
             bonus = deposit / bonus1;
         }
         int finalAccount = originAccount + deposit + bonus;
