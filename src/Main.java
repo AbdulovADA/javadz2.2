2521cb80613fc9dc8c226fbfce7cae859d9edc89
AbdulovADA <lil.vlad7608@gmail.com>
2022-04-25 19:52:37 +0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-int originAccount = 200;
-int deposit = 700;
-int account = originAccount + deposit;
-int bonus1 = 100;
-int bonus = 0;
-if (deposit > 1000) {
-    bonus = deposit / bonus1;
-}
-int finalAccount = originAccount + deposit + bonus;
+        int originAccount = 200;
+        int deposit = 900;
+        int account = originAccount + deposit;
+        int bonus1 = 100;
+        int bonus = 0;
+        if (deposit > 1000) ;
+        {
+            bonus = deposit / bonus1;
+        }
+        int finalAccount = originAccount + deposit + bonus;
 
         System.out.println("Ваш баланс:" + finalAccount);
         System.out.println("Количество бонусных рублей:" + bonus);
